27ec47508eae38ab98e41a18961d5ea6af80bdf7
nanbin <735552503@>
2025-03-25 16:06:32 +0800

    产品类别优化

--- pages/product/category.vue
+++ pages/product/category.vue
@@ -1,9 +1,8 @@
 <template>
 	<z-paging ref="paging" v-model="productCategoryList" @query="queryList">
 		<template #top>
-			<view class="head">
-		</view>
-	
+			<view class="head"></view>
+		</template>
 
 		<view  class="content" v-for="(item,index) in productCategoryList" :key="index" :id="item.id" @click="chooseCategory(item.id,item.category)">
 			<view class="type">{{item.category}}</view>
@@ -13,35 +12,34 @@
 			</view>
 		</view>
 
-	
-		<view class="bottom-button">
-			<view @click="add()" class="add">添加</view>
-			<view  @click="edit()" class="edit">{{editText}}</view>
-		</view>
+		<template #bottom>
+			<view class="bottom-button">
+				<view @click="add()" class="add">添加</view>
+				<view  @click="edit()" class="edit">{{editText}}</view>
+			</view>
+		</template>
 		
 	</z-paging>
 	
 <!--编辑新增弹窗-->
-<view>
-	<view v-show="addCategoryHidden" class="popup_content">
-		<uni-forms  ref="forma" :modelValue="formData" :rules="rules">
+	<uni-popup ref="popup" type="bottom" border-radius="10px 10px 0 0">
+		<view  class="popup_content">			
 			<view class="popup_title">{{addOrEditCateGory}}{{formData.id}}</view>
 			<view class="add-category">
-				<uni-forms-item name="id" v-show="false">
-					<input v-model="formData.id"/>
-				</uni-forms-item>
-				<uni-forms-item name="category">
-					<textarea maxlength="100" v-model="formData.category" placeholder="请输入类别名称" placeholder-class="textarea-placeholder"></textarea>
-				</uni-forms-item>
+				<textarea maxlength="100" v-model="formData.category" 
+					placeholder="请输入类别名称" 
+					placeholder-class="textarea-placeholder">
+				</textarea>
 			</view>
-		</uni-forms>
-		<view class="add-category-foot">
-			<button class="cancel" @click="cancelArrival()">取消</button>
-			<button  class="determine" @click="submit()">确定</button>
+			
+			<view class="add-category-foot">
+				<view class="cancel" @click="cancelArrival()">取消</view>
+				<view  class="determine" @click="submit()">确定</view>
+			</view>
 		</view>
-	</view>
-	<view class="popup_overlay" v-show="addCategoryHidden" @click="hideDiv()"></view>
-</view>
+	</uni-popup>
+	
+
 </template>
 
 <script>
@@ -50,10 +48,8 @@
 		data() {
 			return {
 				showEditAndDelete:true,
-				editText:"编辑",
-				addCategoryHidden:false,
+				editText:"编辑",				
 				productCategoryList:[],				
-				pageNum: 1, // 当前页	
 				addOrEditCateGory:"",
 				formData:{
 					id:"",
@@ -81,13 +77,14 @@
 			},
 			add(){
 				this.addOrEditCateGory = "添加产品类别";
-				this.addCategoryHidden = true;
+				this.$refs.popup.open('center');
+				/* this.addCategoryHidden = true; */
 			},
 			imgEdit(category,id){
 				this.formData.category = category;
 				this.formData.id = id;
 				this.addOrEditCateGory = "修改产品类别";
-				this.addCategoryHidden = true;				
+				/* this.addCategoryHidden = true;	 */			
 			},
 			getList(){
 				post("product/selectCategory",{"pageNum":this.pageNum}).then(res =>{
@@ -117,50 +114,46 @@
 				})
 			},
 			cancelArrival(){
-				this.addCategoryHidden = false;	
+				/* this.addCategoryHidden = false;	 */
 			},	
-			hideDiv(){
+ 			hideDiv(){
 				uni.redirectTo({//跳转当前页面执行刷新
 					url: '/pages/product/category'
 				});
 			},
 			submit(){
-				this.$refs['forma'].validate().then(res=>{
-					if("添加产品类别" == this.addOrEditCateGory){
-						post("product/insertProductCategory",{"category":res.category}).then(res =>{
-							if(200 == res.code){
-								this.formData.category = "";
-								uni.hideLoading();
-								uni.showToast({
-									title: '添加产品类别成功',
-									icon: 'none',
-									duration: 2000
-								})
-								this.addCategoryHidden = false;								
-								this.getList();
-							}
-						})
-					}else if ("修改产品类别" == this.addOrEditCateGory){
-						post("product/updateProductCategory",{"id":res.id,"category":res.category}).then(res =>{
-							if(200 == res.code){
-								this.formData.category = "";
-								uni.hideLoading();
-								uni.showToast({
-									title: '修改产品类别成功',
-									icon: 'none',
-									duration: 2000
-								})
-								this.addCategoryHidden = false;
-								this.getList()
-							}
-						})
-					}
-				}).catch(err =>{
-					console.log('表单错误信息：', err);
-				})
+				let category = this.formData.category;
+				if("添加产品类别" == this.addOrEditCateGory){
+					post("product/insertProductCategory",{"category":category}).then(res =>{
+						if(200 == res.code){
+							this.formData.category = "";
+							uni.hideLoading();
+							uni.showToast({
+								title: '添加产品类别成功',
+								icon: 'none',
+								duration: 2000
+							})
+						}
+					})
+				}else if ("修改产品类别" == this.addOrEditCateGory){
+					post("product/updateProductCategory",{"id":res.id,"category":res.category}).then(res =>{
+						if(200 == res.code){
+							this.formData.category = "";
+							uni.hideLoading();
+							uni.showToast({
+								title: '修改产品类别成功',
+								icon: 'none',
+								duration: 2000
+							})
+							/* this.addCategoryHidden = false; */
+							this.getList()
+						}
+					})
+				}
+				
 
 			},
-			chooseCategory(id,category){				
+			chooseCategory(id,category){
 				let pages= getCurrentPages();
 				let prevPage = pages[pages.length-2]
 				prevPage.$vm.product.productCategoryName  = category;
@@ -169,7 +162,7 @@
 					delta: 1
 				});
 			},
-			onReachBottom() {
+/* 			onReachBottom() {
 				if(this.totalCount > this.productCategoryList.length){
 					this.pageNum++;				
 					post("product/selectCategory",{"pageNum":this.pageNum}).then(res =>{					
@@ -179,7 +172,7 @@
 				}else if(this.totalCount == this.productCategoryList.length){								 
 					 this.status = "noMore"				
 				}
-			},
+			}, */
 			queryList(pageNo, pageSize) {
 				post("product/selectCategory",{"pageNum":pageNo}).then(res =>{
 					this.$refs.paging.complete(res.rows);
@@ -204,7 +197,7 @@
 	}
 </script>
 
-<style>
+<style scoped>
 .head{
 	height: 5px;
 	width: 100%;
@@ -240,8 +233,6 @@
 
 
 .bottom-button{
-	position: fixed;
-	bottom: 0;
 	height: 39px;
 	width: 100%;
 	line-height: 39px;
@@ -250,7 +241,6 @@
 	display: flex;
 	justify-content: space-between;
 	color: #02a5e6ff;
-	 z-index: 999; /* 确保位于最顶层 */
 }
 .add{
 	width: 50%;
@@ -263,51 +253,45 @@
 }
 /* 弹窗 */
  .popup_content {
-	 position: fixed;
-	 top: 50%;
-	 left: 50%;
-	 width: 300px;
-	 height: 260px;
-	 margin-left: -150px;
-	 margin-top: -130px;
-	 border-radius: 10px;
-	 background-color: white;
-	 z-index: 1002;
-	 overflow: auto;
+	position: relative;
+	width: 260px;
+	height: 150px;
+	background-color: white;	
+	overflow: auto;
  }
  .popup_title {
- 	 font-size: 20px;
- 	 font-weight: 500;
+ 	 font-size: 15px;
  	 text-align: center;
- 	 margin: 10px 0;	
+ 	 margin: 5px; 
  }
+ 
 .add-category{
 	margin: 0 10px;
-	padding:10px;
+	padding:5px;
 	border:1px solid #f1f1f1ff;
 	border-radius: 5px;
 }
 .add-category textarea{
-	 height: 90px;
-	 width: 170px;
+	 height: 60px;
+	 width: 230px;
 }
 .textarea-placeholder{
 	font-size: 15px;
 	text-align: left;
-	color: #aaa;	
+	color: #aaa;
 }
 
 .add-category-foot {
-	width: 300px; 
+	width: 260px;
 	position: absolute;
 	bottom: 0;
 	left: 0;
-    height: 50px;
-	line-height: 50px;
+	height: 35px;
+	line-height: 35px;
 	color: #070707ff;
-	font-size: 20px;
+	font-size: 15px;
 	display: flex;
-    border-top: 1px solid #f1f1f1ff;
+	border-top: 1px solid #f1f1f1ff;
 }
 .add-category-foot .cancel{
 	text-align: center;
@@ -324,20 +308,6 @@
 .add-category-foot uni-button:after {
   content: none; /* 移除内容 */
 }
-/**
- * 弹出框遮罩层
- */
-.popup_overlay {
-	 position: fixed;
-	 top: 0%;
-	 left: 0%;
-	 width: 100%;
-	 height: 100%;
-	 background-color: #b3b3b3ff;
-	 z-index: 1001;
-	 -moz-opacity: 0.8;
-	 opacity: .80;
-	 filter: alpha(opacity=88);
- }
+
 
 </style>
